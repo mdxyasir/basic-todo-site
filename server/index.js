1341ec65e5de1aebed020a8cd95ebb5ee946eb50
Yasir <mdxyasir@gmail.com>
2021-11-18 19:59:55 +0000

    removed update todo

--- server/index.js
+++ server/index.js
@@ -44,21 +44,6 @@ app.get("/todos/:id", async (req, res) => {
     }
 })
 
-app.put("/todos/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { description } = req.body;
-        const updateTodo = await pool.query(
-            "UPDATE todo SET description = $1 WHERE id = $2",
-            [description, id]
-        );
-
-        res.json("Todo Updated");
-    } catch(e) {
-        console.log(e.message);
-    }
-})
-
 app.delete("/todos/:id", async (req, res) => {
     try {
         const { id } = req.params;
